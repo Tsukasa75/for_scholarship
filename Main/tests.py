09de80d83d5734f8726af32d3a48eb819fdbe81b
skyriver1126 <sora.17.1126@gmail.com>
2024-02-19 14:50:37 +0900

    テストの修正

--- Main/tests.py
+++ Main/tests.py
@@ -1,122 +1,165 @@
-from django.test import TestCase,Client
-from django.urls import reverse 
-from .models import CustomUser,Address
-from .forms import UserAddressForm
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from .forms import CommentForm, SellForm, UserAddressForm
+from .models import Address, Class, Comment, CustomUser, Like, Product, Transaction
+
 
 class UserAddressFormTests(TestCase):
     def test_valid_post_length(self):
         # 郵便番号が7桁の場合は有効であることを検証する
-        params = {'post': '1234567',}
+        params = {
+            "post": "1234567",
+        }
         form = UserAddressForm(params)
         self.assertTrue(form.is_valid())
 
     def test_invalid_post_length(self):
         # 郵便番号が7桁ではない場合は無効であることを検証する
-        params = {'post': '12345678'}  # 8桁の郵便番号
+        params = {"post": "12345678"}  # 8桁の郵便番号
         form = UserAddressForm(params)
         self.assertFalse(form.is_valid())
 
     def test_invalid_post_when_non_numeric_post(self):
-        params = {'post': 'ABCDEFG'} # 7文字だが数字以外のデータ
+        params = {"post": "ABCDEFG"}  # 7文字だが数字以外のデータ
         form = UserAddressForm(params)
         self.assertFalse(form.is_valid())
 
 
 class TestEditAddressView(TestCase):
     def setUp(self):
-        self.client = Client() 
-        self.user = CustomUser.objects.create_user(username='testuser',password='12345')
-        self.client.login(username='testuser',password='12345')
-        self.url = reverse('edit_address', args=[self.user.username])
-        self.address = Address.objects.create(user=self.user, first_name='Test', last_name='User', post='1000000', prefecture='13', city='Test City', house_number='1-1', building='Test Building')
+        self.client = Client()
+        self.user = CustomUser.objects.create_user(
+            username="testuser", password="12345"
+        )
+        self.client.login(username="testuser", password="12345")
+        self.url = reverse("edit_address", args=[self.user.username])
+        self.address = Address.objects.create(
+            user=self.user,
+            first_name="Test",
+            last_name="User",
+            post="1000000",
+            prefecture="13",
+            city="Test City",
+            house_number="1-1",
+            building="Test Building",
+        )
 
     def test_edit_address_success(self):
-        response = self.client.post(self.url, {
-            'first_name': 'New Test',
-            'last_name': 'User',
-            'post': '1000001',
-            'prefecture': '13',
-            'city': 'New City',
-            'house_number': '1-2',
-            'building': 'New Building'
-        })
+        response = self.client.post(
+            self.url,
+            {
+                "first_name": "New Test",
+                "last_name": "User",
+                "post": "1000001",
+                "prefecture": "13",
+                "city": "New City",
+                "house_number": "1-2",
+                "building": "New Building",
+            },
+        )
 
         self.address.refresh_from_db()
-        self.assertEqual(self.address.first_name, 'New Test')
-        self.assertEqual(self.address.city, 'New City')
-        self.assertEqual(self.address.house_number, '1-2')
+        self.assertEqual(self.address.first_name, "New Test")
+        self.assertEqual(self.address.city, "New City")
+        self.assertEqual(self.address.house_number, "1-2")
         self.assertEqual(response.status_code, 302)
 
     def test_edit_address_failure(self):
-        response = self.client.post(self.url, {
-            'first_name': '',
-            'last_name': 'User',
-            'post': '1000001',
-            'prefecture': '13',
-            'city': '',
-            'house_number': '1-2',
-            'building': 'New Building'
-        })
+        response = self.client.post(
+            self.url,
+            {
+                "first_name": "",
+                "last_name": "User",
+                "post": "1000001",
+                "prefecture": "13",
+                "city": "",
+                "house_number": "1-2",
+                "building": "New Building",
+            },
+        )
 
         self.assertEqual(response.status_code, 200)
-        self.assertFormError(response, 'user_form', 'first_name', 'This field is required.')
-        self.assertFormError(response, 'user_form', 'city', 'This field is required.')
+        self.assertFormError(
+            response, "user_form", "first_name", "This field is required."
+        )
+        self.assertFormError(response, "user_form", "city", "This field is required.")
 
-class TestCreateCardView(TestCase): 
-    def setUp(self): 
-        self.client=Client() 
-        self.user=CustomUser.objects.create_user(username='testuser',password='12345') 
-        self.client.login(username='testuser',password='12345')
-        self.url = reverse('create_card', args=[self.user.username])
+
+class TestCreateCardView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = CustomUser.objects.create_user(
+            username="testuser", password="12345"
+        )
+        self.client.login(username="testuser", password="12345")
+        self.url = reverse("create_card", args=[self.user.username])
 
     def test_create_card_success(self):
-        response = self.client.post(self.url, {
-            'card_number': '4242424242424242',
-            'exp_month': '12',
-            'exp_year': '2023',
-            'cvc': '123'
-        })
+        response = self.client.post(
+            self.url,
+            {
+                "card_number": "4242424242424242",
+                "exp_month": "12",
+                "exp_year": "2023",
+                "cvc": "123",
+            },
+        )
 
         self.assertEqual(response.status_code, 302)
 
     # まだ短い
     def test_create_card_failure(self):
-        response = self.client.post(self.url, {
-            'card_number': '1234567890123456',
-            'exp_month': '12', 
-        })  
+        self.client.post(
+            self.url,
+            {
+                "card_number": "1234567890123456",
+                "exp_month": "12",
+            },
+        )
 
-class TestPaymentView(TestCase): 
-    def setUp(self): 
-        self.client=Client() 
-        self.user=CustomUser.objects.create_user(username='testuser',password='12345') 
-        self.client.login(username='testuser',password='12345')
 
-        self.url = reverse('payment', args=[self.user.username])
+class TestPaymentView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = CustomUser.objects.create_user(
+            username="testuser", password="12345"
+        )
+        self.client.login(username="testuser", password="12345")
+
+        self.url = reverse("payment", args=[self.user.username])
 
     def test_payment_success(self):
-       response = self.client.post(self.url, {
-           'card_number': '0', 
-           'amount': '2000'
-       })
-
-       self.assertEqual(response.status_code, 302)
-       self.assertRedirects(response, reverse('payment_complete'))
-
-    def test_payment_failure(self):
-        response = self.client.post(self.url, {
-            'card_number': '1', 
-            'amount': '2000',
-        })
+        response = self.client.post(self.url, {"card_number": "0", "amount": "2000"})
 
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('payment'))
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
-from django.urls import reverse
+        self.assertRedirects(response, reverse("payment_complete"))
 
-from .forms import SellForm
-from .models import Address, Class, CustomUser, Product, Transaction
+    def test_payment_failure(self):
+        response = self.client.post(
+            self.url,
+            {
+                "card_number": "1",
+                "amount": "2000",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("payment"))
+
+
+class commentFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = CommentForm()
+        self.assertIn("text", form.fields)
+
+    def test_valid_data(self):
+        form_data = {
+            "text": "text_text",
+        }
+        form = CommentForm(form_data)
+        self.assertTrue(form.is_valid())
 
 
 class sellFormTest(TestCase):
@@ -393,7 +436,7 @@ class descriptionViewTest(TestCase):
         cls.Address1 = Address.objects.create(
             user=cls.user1,
             post=1111111,
-            prefecture="大阪府",
+            prefecture="東京都",
             city="1",
             house_number=1,
             building="test",
@@ -492,3 +535,25 @@ class descriptionViewTest(TestCase):
     def test_sold_product(self):
         response = self.client.get("/product_description/3/")
         self.assertEqual(response.status_code, 200)
+
+    def test_like_product(self):
+        self.client.post(
+            reverse("like_product"),
+            {"product_pk": 1},
+        )
+        like = Like.objects.get(user=self.user1, product=self.pro1)
+        self.assertIsNotNone(like)
+
+    def test_unlike_product(self):
+        Like.objects.create(user=self.user1, product=self.pro1)
+        self.client.post(
+            reverse("like_product"),
+            {"product_pk": 1},
+        )
+        unlike = Like.objects.filter(user=self.user1, product=self.pro1).first()
+        self.assertIsNone(unlike)
+
+    def test_comment_create(self):
+        comment = Comment.objects.filter(user=self.user1).first()
+        self.assertIsNotNone(comment)
+        self.assertEqual(comment.text, "test_comment")
